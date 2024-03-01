df450fe822d0de81401c09d268ca10a275425284
Shadient <85789237+Shadient@users.noreply.github.com>
2024-03-01 12:39:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,17 +12,17 @@ echo
 echo "INSTALLING DEPENDENCIES"
 echo
 
-PKGSAUR=(
+PKGSDEP=(
   'base-devel'
-  'curl'
-  'less'
-  'ripgrep'
-  'wget'
+  'glfw-x11'
+  'phonon-qt5-gstreamer'
 )
 
-for PKGAUR in "${PKGSAUR[@]}"; do
-  echo "INSTALLING: ${PKGAUR}"
-  sudo pacman -S "$PKGAUR" --needed
+for PKGDEP in "${PKGSDEP[@]}"; do
+  echo
+  echo "INSTALLING: ${PKGDEP}"
+  echo
+  sudo pacman -S "$PKGDEP" --needed
 done
 
 # Install Node Version Manager
@@ -42,21 +42,15 @@ echo
 
 PKGS=(
 
-  # -- AUDIO -- #
-
+  # -- AUDIO --
   'alsa-utils'
   'alsa-plugins'
+  'alsa-firmware'
+  'pavucontrol'
   'pulseaudio'
   'pulseaudio-alsa'
-  'pavucontrol'
 
-  # -- COMMUNICATIONS --
-
-  'discord'
-  'telegram-desktop'
-
-  # -- GPARTED --
-
+  # -- GPARTED & DEPENDENCIES --
   'gparted'
   'btrfs-progs'   # for btrfs partitions
   'dosfstools'    # for FAT16 and FAT32 partitions
@@ -70,42 +64,58 @@ PKGS=(
   'reiserfsprogs' # for reiser partitions
   'udftools'      # for UDF file system support
   'xfsprogs'      # for xfs partitions
-
+  
   # -- MISC --
-
+  'acpi'
   'alacritty'
   'bitwarden'
   'blender'
-  'cmatrix'
+  'brightnessctl'
+  'btop'
+  'chromium'
+  'curl'
+  'cmatrix'  
+  'discord'
+  'firefox'
   'flameshot'
+  'git'
+  'gtop'
+  'htop'
   'kate'
+  'less'
+  # 'linux419-virtualbox-host-modules'
   'man-db'
   'meson'
+  'neofetch'
+  'neovim'
   'networkmanager'
   'obsidian'
   'okular'
+  'opera'
+  'perl'
   'polkit'
   'python-pip'
   'qbittorrent'
+  'qt5-base'
+  'qt6-base'
   'redshift'
+  'ripgrep'
   'rust'
   'tectonic'
+  'telegram-desktop'
+  'ttf-jetbrains-mono-nerd'
   'thunar'
+  # 'virtualbox'
   'vlc'
+  'wget'
+  'xdg-user-dirs'
   'zathura'
-
-  # -- TERMINAL UTILITIES --
-
-  'btop'
-  'brightnessctl'
-  'gtop'
-  'htop'
-  'neofetch'
-
 )
 
 for PKG in "${PKGS[@]}"; do
+  echo
   echo "INSTALLING: ${PKG}"
+  echo
   sudo pacman -S "$PKG" --needed
 done
 
@@ -129,7 +139,6 @@ source ~/.bashrc
 paru -S etcher-bin
 paru -S vscodium-bin
 paru -S waterfox-bin
-paru -S eww
 paru -S bitwig-studio
 
 #-------------------------------------------------------------------------
